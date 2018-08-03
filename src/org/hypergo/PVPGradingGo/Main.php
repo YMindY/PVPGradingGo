01ea70689d281dd21cf8bf8f00be04867edbce2c
YxMingY <526817087@qq.com>
2018-08-03 22:37:04 +0800

    emm

---
+++ src/org/hypergo/PVPGradingGo/Main.php
@@ -0,0 +1,14 @@
+<?php
+namespace org\hypergo\PVPGradingGo;
+use pocketmine\plugin\PluginBase; 
+class Main extends PluginBase{
+   public function onLoad(){
+      $this->getLogger()->info("正在加载!");
+   }
+   public function onEnable(){
+      $this->getLogger()->notice("插件 已启动! \n§b开发人员: HypergoStdio(chy23333,xMing)");
+   }
+   public function onDisable(){
+      $this->getLogger()->warning("插件 已关闭!");
+   }
+}
