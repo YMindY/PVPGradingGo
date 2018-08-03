a74b58c5d9a900805c441f4883b23896ebc649a0
chy23333 <1017354253@qq.com>
2018-08-03 22:49:33 +0800

    update Main.php

--- src/org/hypergo/PVPGradingGo/Main.php
+++ src/org/hypergo/PVPGradingGo/Main.php
@@ -6,7 +6,7 @@ class Main extends PluginBase{
       $this->getLogger()->info("正在加载!");
    }
    public function onEnable(){
-      $this->getLogger()->notice("插件 已启动! \n§b开发人员: HypergoStdio(chy23333,xMing)");
+      $this->getLogger()->notice("插件 已启动! \n§b开发人员: HypergoStdio(Creay,xMing)");
    }
    public function onDisable(){
       $this->getLogger()->warning("插件 已关闭!");
